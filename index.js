ee3861e16be482d262b5cf751206645770ca7032
ahsantawil <ahsan.tawil07@gmail.com>
2022-06-05 18:54:50 +0700

    Challenge callback

--- index.js
+++ index.js
@@ -6,15 +6,34 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = ( name, sortName) => {
+  let output = sortName(name);
+
+  const number = [];
+  for (const data of name) {
+    number.push(name.indexOf(data) + 1);
+  }
+
+  let arrayName = [];
+  for (let i = 0; i < name.length; i++) {
+    arrayName.push(`${number[i]}. ${output[i]}`);
+    
+  }
+
+  return arrayName;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = ( name ) => {
+  return name.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = ( name ) => {
+  return name.reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
